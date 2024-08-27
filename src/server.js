a169e985c497b0141b95a2a4e34de88f940f1466
guitiorr <officialglude12345@gmail.com>
2024-08-27 11:39:05 +0700

    fix 2

--- src/server.js
+++ src/server.js
@@ -2,20 +2,15 @@ const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 
 const init = async () => {
-  const server = Hapi.server({
+  const server = Hapi.server({    
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-      cors: {
-        origin: ['*'], // Allow all origins
-        //headers: ['Accept', 'Content-Type'], // Specify which headers can be accepted
-        // exposedHeaders: ['Accept', 'Content-Type'], // Specify which headers are exposed
-        // additionalExposedHeaders: ['X-Custom-Header'], // Any additional headers you want to expose
-        // maxAge: 60,
-        // credentials: true // If you need to include credentials like cookies in CORS requests
-      }
-    }
-  });
+        cors: {
+            origin: ['*'],
+        },
+    },
+});
 
   server.route(routes);
 
